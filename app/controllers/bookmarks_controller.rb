3910cb1a188d4f6f2cecfeb5f41fa300a91695c4
walker-cook <jwcook99@gmail.com>
2022-03-02 01:55:14 +0000

    DOne

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,60 @@
+class BookmarksController < ApplicationController
+  def index
+    u_id = session[:user_id]
+    matching_bookmarks = Bookmark.where({:user_id => u_id})
+    @list_of_movies = Movie.all
+
+    @current_user = User.where({ :id => u_id}).at(0)
+
+    @list_of_bookmarks = matching_bookmarks.order({ :created_at => :desc })
+
+    render({ :template => "bookmarks/index.html.erb" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_bookmarks = Bookmark.where({ :id => the_id })
+
+    @the_bookmark = matching_bookmarks.at(0)
+
+    render({ :template => "bookmarks/show.html.erb" })
+  end
+
+  def create
+    the_bookmark = Bookmark.new
+    the_bookmark.user_id = params.fetch("query_user_id")
+    the_bookmark.movie_id = params.fetch("query_movie_id")
+
+    if the_bookmark.valid?
+      the_bookmark.save
+      redirect_to("/bookmarks", { :notice => "Bookmark created successfully." })
+    else
+      redirect_to("/bookmarks", { :notice => "Bookmark failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_bookmark = Bookmark.where({ :id => the_id }).at(0)
+
+    the_bookmark.user_id = params.fetch("query_user_id")
+    the_bookmark.movie_id = params.fetch("query_movie_id")
+
+    if the_bookmark.valid?
+      the_bookmark.save
+      redirect_to("/bookmarks/#{the_bookmark.id}", { :notice => "Bookmark updated successfully."} )
+    else
+      redirect_to("/bookmarks/#{the_bookmark.id}", { :alert => "Bookmark failed to update successfully." })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_bookmark = Bookmark.where({ :id => the_id }).at(0)
+
+    the_bookmark.destroy
+
+    redirect_to("/bookmarks", { :notice => "Bookmark deleted successfully."} )
+  end
+end
